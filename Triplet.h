fe6d5f8f1d4f10ad2133161676b97c568790eb90
baraveh <baraveh@gmail.com>
2019-06-06 10:29:12 +0300

    potato

--- Triplet.h
+++ Triplet.h
@@ -24,9 +24,9 @@ public:
     }
 
     bool operator==(const Triplet &aTriplet) {
-        return t_first =
-               aTriplet.p_first && t_second =
-                       aTriplet.p_second && t_third == aTriplet.t_third;
+        return t_first ==
+               aTriplet.t_first && t_second =
+                       aTriplet.t_second && t_third == aTriplet.t_third;
     }
 
     bool operator<(const Triplet &aTriplet) {

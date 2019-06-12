b9c42789db136928bb4080b358023367d7ec05e8
baraveh <baraveh@gmail.com>
2019-06-12 13:45:12 +0300

    testing the issue in gal's test

--- Tests/roomHashTests.cpp
+++ Tests/roomHashTests.cpp
@@ -6,15 +6,8 @@
 
 int main(){
     RoomHash hash = RoomHash();
-    hash.addRoom(1);
-    Room room1 = hash.find(1);
-    bool test = room1.addLecture(1,234218, 12);
-    assert(test);
-    room1.removeLecture(1);
-    hash.addRoom(2);
-    hash.addRoom(3);
-    hash.find(2);
-    hash.find(3);
-    hash.removeRoom(2);
-    hash.removeRoom(3);
+    hash.addRoom(5);
+    hash.addRoom(10);
+    hash.addRoom(12);
+    hash.addRoom(13);
 }
